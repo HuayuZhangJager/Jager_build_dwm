ef46d04eca76d92634e052beee861bfae21fd6a1
Huayu Zhang <zhanghuayu1233@qq.com>
2022-05-18 15:05:36 +0100

    update

--- script/startdwm.sh
+++ script/startdwm.sh
@@ -6,4 +6,11 @@ slstatus &
 xautolock -time 5 -locker slock &
 feh --bg-scale /usr/share/wallpapers/KaliLayers/contents/images/3840x2160.png &
 xrandr --output DP-2 --mode 1920x1080 --rate 240 &
+# restart dwm without closing other x program
+while true; do
+	# Log stderror to a file 
+	dwm 2> ~/.dwm.log
+	# No error logging
+	#dwm >/dev/null 2>&1
+done
 exec dwm 
